a0973e96402a3bbbce465b9e0b47a8fdf38ab1e4
kaynan <78617015+kaynanxd@users.noreply.github.com>
2025-12-06 22:09:33 -0300

    Delete schemas directory

--- schemas/common.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, Field
-
-class Message(BaseModel):
-    message: str
-
-class FilterPage(BaseModel):
-    offset: int = Field(0, ge=0)
-    limit: int = Field(100, ge=1, le=100)
