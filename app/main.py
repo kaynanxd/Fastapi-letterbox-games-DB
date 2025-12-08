f7881bd35bfc6ade190226dd50520379acc14cf9
kaynan <78617015+kaynanxd@users.noreply.github.com>
2025-12-08 19:01:56 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,15 +6,27 @@ from fastapi import FastAPI
 
 from app.routers import auth, watchlist, users
 from app.schemas.common import Message
-
 from fastapi.staticfiles import StaticFiles
+
+from fastapi.middleware.cors import CORSMiddleware
 
 if sys.platform == 'win32':
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-
+origins = [
+    "http://localhost:5173",    
+    "http://127.0.0.1:5173",    
+]
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,      
+    allow_credentials=True,
+    allow_methods=["*"],        
+    allow_headers=["*"],        
+)
 
 app.mount("/media", StaticFiles(directory="media"), name="media")
 
